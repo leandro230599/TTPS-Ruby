d9f27312a038abc72568a79771442999d1f7b900
Leandro Lopez <leandro230599@outlook.com>
2022-09-21 01:36:56 -0300

    Terminado ejercicio 1 Practica 2

---
+++ Practica 2/ejercicio1.rb
@@ -0,0 +1,9 @@
+# Tengo que hacer un metodo que recibe un arreglo y lo ordena y devuelve
+# ordenar_arreglo([1, 4, 6, 2, 3, 0, 10])
+# => [0, 1, 2, 3, 4, 6, 10]
+
+def ordenar_arreglo(arr)
+    return arr.sort
+end
+
+p ordenar_arreglo([1, 4, 6, 2, 3, 0, 10])
