df932dded7db46a20259fb69eaf1384cb69fcaf5
Leandro Lopez <leandro230599@outlook.com>
2022-09-20 23:50:55 -0300

    Terminado ejercicio 17 Practica 1

---
+++ Practica 1/ejercicio17.rb
@@ -0,0 +1,9 @@
+# Pide crear un script para que al ejecutar este programa por consola, solicite un nombre y al escribirlo por teclado, lo imprime en pantalla
+# $ ruby script.rb
+#Por favor, ingresá tu nombre:
+#R2-D2
+#¡Hola, R2-D2!
+
+puts "Por favor, ingresá tu nombre: "
+a = gets.chomp
+puts "¡Hola, #{a}!"
